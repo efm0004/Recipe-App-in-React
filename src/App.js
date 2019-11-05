5503dcf8f436a2b4cf131b9bfdff62d2f9d7a709
Emily Moore <efm0004@gmail.com>
2019-11-05 14:25:12 -0600

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = process.env.APP_ID;
   const APP_KEY = process.env.APP_KEY;
-
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
+  useEffect(() =>{
+    console.log('Effect has been run');
+  })
 
   return(
     <div className='App'>
