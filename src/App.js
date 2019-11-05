46dc76c186bd2deae9463b8f3bcbe097c2831ea5
Emily Moore <efm0004@gmail.com>
2019-11-05 14:03:49 -0600

    update app_id and app_key

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 const App = () => {
 
-  const APP_ID = {process.env.APP_ID};
-  const APP_KEY = {process.env.APP_KEY};
+  const APP_ID = process.env.APP_ID;
+  const APP_KEY = process.env.APP_KEY;
 
   return(
     <div className='App'>
