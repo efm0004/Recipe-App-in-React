fbc07c45b2ab425da72c466afad43338d6d3c2a0
Emily Moore <efm0004@gmail.com>
2019-11-05 14:01:51 -0600

    edamam ID & Key updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 const App = () => {
 
+  const APP_ID = {process.env.APP_ID};
+  const APP_KEY = {process.env.APP_KEY};
+
   return(
     <div className='App'>
       <h1>Hello React</h1>
