2052541156a2e3726b1c60f9dcd251cc7a3f3b81
Emily Moore <efm0004@gmail.com>
2019-11-05 15:21:33 -0600

    update search input

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect( () => {
     getRecipes();
@@ -21,10 +22,18 @@ const App = () => {
     console.log(data.hits);
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
   return(
     <div className='App'>
       <form className="search-form">
-        <input className="search-bar" type='text'></input>
+        <input 
+        className="search-bar" 
+        type='text' 
+        value={search} 
+        onChange={updateSearch}></input>
         <button className="search-button" type='submit'>Search</button>
       </form>
       {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> */}
